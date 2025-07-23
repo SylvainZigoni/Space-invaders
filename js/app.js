28668e34e5bf0714c9a87f19a8e73066b323cebb
AnnePopoolaRousson <anne.popoola-rousson@oclock.school>
2025-07-23 14:49:14 +0200

    Etape 4, exercice fini

--- js/app.js
+++ js/app.js
@@ -6,8 +6,6 @@ const container = document.querySelector("#invader");
 // On fait une boucle pour créer les 8 lignes
 for (let i = 1; i <= 8; i++) {
     // On déclare la constante line dans la boucle pour ne pas qu'elle s'écrase et se crée toujours au meme endroit
-    console.log("Je suis dans la boucle");
-    console.log(i);
     const line = document.createElement("p");
     line.classList.add("ligne");
     container.append(line);
@@ -27,15 +25,13 @@ function handlePixel() {
 }
 
 function handleClass() {
+    // On execute handlepixel pour mettre en variable le tableau de tous nos pixels de la feuille    pixels = handlePixel();
     pixels = handlePixel();
-
     // On fait une boucle sur l'ensemble des pixel de notre tableau pixels. (Ca permet de ne pas utiliser forEach)
     for (const pixelElt of pixels) {
         pixelElt.addEventListener("click", () => {
-            console.dir(pixelElt);
             // Comme notre pixel a 2 classes dans le if pour voir s'il a le nom black pixel, il faut bien mettre class1 + class2.
             if (pixelElt.className == "pixel black-pixel") {
-                console.log(pixelElt);
                 pixelElt.classList.remove("black-pixel");
             } else {
                 pixelElt.classList.add("black-pixel");
@@ -56,7 +52,7 @@ formulaire.appendChild(sizePixelElt);
 formulaire.appendChild(buttonElt);
 
 sizeFormElt.setAttribute("placeholder", "Taille de la grille");
-sizePixelElt.setAttribute("placeholder", "Taille des pixels");
+sizePixelElt.setAttribute("placeholder", "Taille des pixels, minimum 5");
 buttonElt.textContent = "Valider";
 
 sizeFormElt.classList.add("form-grille");
@@ -81,7 +77,6 @@ formulaire.addEventListener("submit", (event) => {
 
 function userInputSize() {
     const userSize = sizeFormElt.value;
-    console.log(userSize);
     return userSize;
 }
 
@@ -89,33 +84,46 @@ function userInputSize() {
 
 function userInputPixel() {
     const userPixel = sizePixelElt.value;
-    console.log(userPixel);
     return userPixel;
 }
 
 //Créer une fonction pour ajouter une ligne
 
 function addUserInput() {
-    removeAll();
+    //On appelle les fonctions qui ont demandées à l'utilisateur une taille de grille et un nombre de pixel
+    //Et on récupère les valeurs indiquées dans des variables
     const numero = userInputSize();
-    for (let i = 1; i <= numero; i++) {
-        // On déclare la constante line dans la boucle pour ne pas qu'elle s'écrase et se crée toujours au meme endroit
-        console.log("Je suis dans la boucle");
-        console.log(i);
-        const line = document.createElement("p");
-        line.classList.add("ligne");
-        container.append(line);
-        // On fait une boucle pour créer les 8 pixels
-        for (let j = 1; j <= numero; j++) {
-            // On déclare la constante pixel dans la boucle pour ne pas qu'elle s'écrase et se crée toujours au meme endroit
-            const pixel = document.createElement("div");
-            pixel.classList.add("pixel");
-            line.append(pixel);
+    const size = userInputPixel();
+    //Si les valeurs ne sont pas des numéros ou sont des valeurs trop petites on retourne un message d'erreur
+    //Et on sort de la fonction
+    if (isNaN(numero) || isNaN(size) || numero < 1 || size < 4) {
+        alert("Mauvaise saisie.");
+        return;
+        //Sinon on modifie notre grille en fonction des paramètres donnés par l'utilisateur
+    } else {
+        removeAll();
+        for (let i = 1; i <= numero; i++) {
+            // On déclare la constante line dans la boucle pour ne pas qu'elle s'écrase et se crée toujours au meme endroit
+            const line = document.createElement("p");
+            line.classList.add("ligne");
+            container.append(line);
+            // On fait une boucle pour créer les 8 pixels
+            for (let j = 1; j <= numero; j++) {
+                // On déclare la constante pixel dans la boucle pour ne pas qu'elle s'écrase et se crée toujours au meme endroit
+                const pixel = document.createElement("div");
+                pixel.classList.add("pixel");
+                //On modifie la taille du pixel en changeant le style en fonction de la valeur donnée par l'utilisateur
+                pixel.style.height = size + "px";
+                pixel.style.width = size + "px";
+                line.append(pixel);
+            }
         }
+        //On appelle la fonction qui gère le changement de couleur des pixels
+        handleClass();
     }
-    handleClass();
 }
 
+//On execute la fonction qui modifie la grille grace aux données demandées par l'utilisateuren cliquant sur le bouton
 buttonElt.addEventListener("click", addUserInput);
 
 //Créer une fonction pour ajouter un pixel de la taille demandée (taille du pixel)
