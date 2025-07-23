8d73e40ba8aa00454eab2d791aa06ac95c0971cd
AnnePopoolaRousson <anne.popoola-rousson@oclock.school>
2025-07-23 10:07:20 +0200

    Etape 1

--- js/app.js
+++ js/app.js
@@ -1,1 +1,21 @@
 // ton code ici
+const container = document.querySelector('#invader');
+
+for(let i = 1; i <= 8; i++) {
+    console.log("Je suis dans la boucle");
+    console.log(i);
+    const line = document.createElement('p');
+    line.classList.add('ligne');
+    container.append(line);
+    for(let j = 1; j <= 8; j++) {
+        const pixel = document.createElement('div');
+        pixel.classList.add('pixel');
+        line.append(pixel);
+    }
+}
+
+
+
+// const pixel = document.createElement('div');
+
+
